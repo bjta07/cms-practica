4fb314e4b2dcf9c91150fe217b5028cafc2053c9
bjta07 <bjta07@gmail.com>
2025-11-10 00:40:45 -0400

    UPDATE FUNCTION

--- frontend/frontend/src/lib/get-article.js
+++ frontend/frontend/src/lib/get-article.js
@@ -1,7 +1,5 @@
 import { query } from './strapi'
 
-const { STRAPI_HOST } = process.env
-
 export function getArticles() {
   return query('articles?populate=cover').then((res) => {
     return res.data.map((article) => {
