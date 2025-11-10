c9fda8823a19841aa3cdee3059859bcf3fa04150
bjta07 <bjta07@gmail.com>
2025-11-09 23:57:40 -0400

    add url

--- backend/backend/config/server.js
+++ backend/backend/config/server.js
@@ -4,7 +4,8 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('RENDER_EXTERNAL_URL', 'https://cms-practica-1.onrender.com'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-});
+})
